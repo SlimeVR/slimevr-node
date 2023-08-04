037b41cdb9db77fb78a834350a42ec3f8550189b
DevMiner <devminer@devminer.xyz>
2023-08-04 17:05:35 +0200

    chore: fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,20 @@ module.exports = {
   root: true,
   extends: ['@slimevr/eslint-config'],
   parserOptions: {
-    project: ['./packages/*/tsconfig.json', './apps/*/tsconfig.json']
+    project: [
+      './packages/*/tsconfig.json',
+      './apps/*/tsconfig.json',
+      './apps/firmware-protocol-debugger-gui/backend/tsconfig.json'
+    ]
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }
+    ]
   }
 };
--- apps/firmware-protocol-debugger-gui/.eslintrc.js
+++ apps/firmware-protocol-debugger-gui/.eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: ['@slimevr/eslint-config', 'react-app']
+  extends: ['@slimevr/eslint-config', 'plugin:react/recommended'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/no-unknown-property': ['error', { ignore: ['args', 'position'] }]
+  }
 };
