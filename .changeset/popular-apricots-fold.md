85df67239518509cc51d704c69ea5619e35634e2
DevMiner <devminer@devminer.xyz>
2023-08-05 14:37:30 +0200

    fix(firmware-protocol-debugger-gui): GUI not loading

---
+++ .changeset/popular-apricots-fold.md
@@ -0,0 +1,5 @@
+---
+'slimevr-firmware-protocol-debugger-gui': patch
+---
+
+fix GUI not loading
--- apps/firmware-protocol-debugger-gui/vite.config.js
+++ apps/firmware-protocol-debugger-gui/vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()]
+  base: process.env.IS_DEV !== 'true' ? './' : '/',
+  plugins: [react()],
+  build: {
+    outDir: path.join(__dirname, 'build')
+  }
 });
